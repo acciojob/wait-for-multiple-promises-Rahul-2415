8f226aabd64d32619f3c0626bf59311bb438eccd
Rahul-2415 <rahulsingh8171335162@gmail.com>
2025-12-29 15:55:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
 //your JS code here. If required.
+
+
+const output = document.getElementById("output");
+
+
+const loadingRow = document.createElement("tr");
+const loadingCell = document.createElement("td");
+loadingCell.colSpan = 2;
+loadingCell.innerText = "Loading...";
+loadingRow.appendChild(loadingCell);
+output.appendChild(loadingRow);
+
+
+function createPromise() {
+  const delay = Math.random() * 2 + 1; 
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(delay);
+    }, delay * 1000);
+  });
+}
+
+
+const p1 = createPromise();
+const p2 = createPromise();
+const p3 = createPromise();
+
+
+Promise.all([p1, p2, p3]).then((times) => {
+  
+  output.innerHTML = "";
+
+  let maxTime = 0;
+
+  times.forEach((time, index) => {
+    const row = document.createElement("tr");
+
+    const nameCell = document.createElement("td");
+    nameCell.innerText = `Promise ${index + 1}`;
+
+    const timeCell = document.createElement("td");
+    timeCell.innerText = time.toFixed(3);
+
+    row.appendChild(nameCell);
+    row.appendChild(timeCell);
+    output.appendChild(row);
+
+    if (time > maxTime) {
+      maxTime = time;
+    }
+  });
+
+  
+  const totalRow = document.createElement("tr");
+
+  const totalName = document.createElement("td");
+  totalName.innerText = "Total";
+
+  const totalTime = document.createElement("td");
+  totalTime.innerText = maxTime.toFixed(3);
+
+  totalRow.appendChild(totalName);
+  totalRow.appendChild(totalTime);
+  output.appendChild(totalRow);
+});
